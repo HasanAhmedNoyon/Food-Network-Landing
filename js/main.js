1d97944d8a6e225cf9008cdfb21b842984247fbe
HasanAhmedNoyon <mohammedhasan9047@gmail.com>
2021-08-01 21:21:03 +0600

    8

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $(".owl-carousel").owlCarousel({
         items: 2,
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 1,
+                nav: false
+            },
+            767: {
+                items: 1,
+                nav: false
+            },
+            1000: {
+                items: 2,
+                nav: true,
+                loop: false
+            }
+        }
     });
-  }); 
-  
-  
-  
+});
